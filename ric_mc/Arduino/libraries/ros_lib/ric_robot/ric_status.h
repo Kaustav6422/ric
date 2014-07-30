a39fac80b0bd9a595529882723975c0477623889
Yam Geva <yam@robotican.net>
2014-07-30 13:51:45 +0300

    fixes

--- ric_mc/Arduino/libraries/ros_lib/ric_robot/ric_status.h
+++ ric_mc/Arduino/libraries/ros_lib/ric_robot/ric_status.h
@@ -13,9 +13,8 @@ namespace ric_robot
   {
     public:
       uint8_t faults;
-      float battery1_voltage;
-      float battery2_voltage;
-      float battery3_voltage;
+      float sensors_battery_voltage;
+      float rover_battery_voltage;
 
     virtual int serialize(unsigned char *outbuffer) const
     {
@@ -25,33 +24,23 @@ namespace ric_robot
       union {
         float real;
         uint32_t base;
-      } u_battery1_voltage;
-      u_battery1_voltage.real = this->battery1_voltage;
-      *(outbuffer + offset + 0) = (u_battery1_voltage.base >> (8 * 0)) & 0xFF;
-      *(outbuffer + offset + 1) = (u_battery1_voltage.base >> (8 * 1)) & 0xFF;
-      *(outbuffer + offset + 2) = (u_battery1_voltage.base >> (8 * 2)) & 0xFF;
-      *(outbuffer + offset + 3) = (u_battery1_voltage.base >> (8 * 3)) & 0xFF;
-      offset += sizeof(this->battery1_voltage);
+      } u_sensors_battery_voltage;
+      u_sensors_battery_voltage.real = this->sensors_battery_voltage;
+      *(outbuffer + offset + 0) = (u_sensors_battery_voltage.base >> (8 * 0)) & 0xFF;
+      *(outbuffer + offset + 1) = (u_sensors_battery_voltage.base >> (8 * 1)) & 0xFF;
+      *(outbuffer + offset + 2) = (u_sensors_battery_voltage.base >> (8 * 2)) & 0xFF;
+      *(outbuffer + offset + 3) = (u_sensors_battery_voltage.base >> (8 * 3)) & 0xFF;
+      offset += sizeof(this->sensors_battery_voltage);
       union {
         float real;
         uint32_t base;
-      } u_battery2_voltage;
-      u_battery2_voltage.real = this->battery2_voltage;
-      *(outbuffer + offset + 0) = (u_battery2_voltage.base >> (8 * 0)) & 0xFF;
-      *(outbuffer + offset + 1) = (u_battery2_voltage.base >> (8 * 1)) & 0xFF;
-      *(outbuffer + offset + 2) = (u_battery2_voltage.base >> (8 * 2)) & 0xFF;
-      *(outbuffer + offset + 3) = (u_battery2_voltage.base >> (8 * 3)) & 0xFF;
-      offset += sizeof(this->battery2_voltage);
-      union {
-        float real;
-        uint32_t base;
-      } u_battery3_voltage;
-      u_battery3_voltage.real = this->battery3_voltage;
-      *(outbuffer + offset + 0) = (u_battery3_voltage.base >> (8 * 0)) & 0xFF;
-      *(outbuffer + offset + 1) = (u_battery3_voltage.base >> (8 * 1)) & 0xFF;
-      *(outbuffer + offset + 2) = (u_battery3_voltage.base >> (8 * 2)) & 0xFF;
-      *(outbuffer + offset + 3) = (u_battery3_voltage.base >> (8 * 3)) & 0xFF;
-      offset += sizeof(this->battery3_voltage);
+      } u_rover_battery_voltage;
+      u_rover_battery_voltage.real = this->rover_battery_voltage;
+      *(outbuffer + offset + 0) = (u_rover_battery_voltage.base >> (8 * 0)) & 0xFF;
+      *(outbuffer + offset + 1) = (u_rover_battery_voltage.base >> (8 * 1)) & 0xFF;
+      *(outbuffer + offset + 2) = (u_rover_battery_voltage.base >> (8 * 2)) & 0xFF;
+      *(outbuffer + offset + 3) = (u_rover_battery_voltage.base >> (8 * 3)) & 0xFF;
+      offset += sizeof(this->rover_battery_voltage);
       return offset;
     }
 
@@ -63,41 +52,30 @@ namespace ric_robot
       union {
         float real;
         uint32_t base;
-      } u_battery1_voltage;
-      u_battery1_voltage.base = 0;
-      u_battery1_voltage.base |= ((uint32_t) (*(inbuffer + offset + 0))) << (8 * 0);
-      u_battery1_voltage.base |= ((uint32_t) (*(inbuffer + offset + 1))) << (8 * 1);
-      u_battery1_voltage.base |= ((uint32_t) (*(inbuffer + offset + 2))) << (8 * 2);
-      u_battery1_voltage.base |= ((uint32_t) (*(inbuffer + offset + 3))) << (8 * 3);
-      this->battery1_voltage = u_battery1_voltage.real;
-      offset += sizeof(this->battery1_voltage);
+      } u_sensors_battery_voltage;
+      u_sensors_battery_voltage.base = 0;
+      u_sensors_battery_voltage.base |= ((uint32_t) (*(inbuffer + offset + 0))) << (8 * 0);
+      u_sensors_battery_voltage.base |= ((uint32_t) (*(inbuffer + offset + 1))) << (8 * 1);
+      u_sensors_battery_voltage.base |= ((uint32_t) (*(inbuffer + offset + 2))) << (8 * 2);
+      u_sensors_battery_voltage.base |= ((uint32_t) (*(inbuffer + offset + 3))) << (8 * 3);
+      this->sensors_battery_voltage = u_sensors_battery_voltage.real;
+      offset += sizeof(this->sensors_battery_voltage);
       union {
         float real;
         uint32_t base;
-      } u_battery2_voltage;
-      u_battery2_voltage.base = 0;
-      u_battery2_voltage.base |= ((uint32_t) (*(inbuffer + offset + 0))) << (8 * 0);
-      u_battery2_voltage.base |= ((uint32_t) (*(inbuffer + offset + 1))) << (8 * 1);
-      u_battery2_voltage.base |= ((uint32_t) (*(inbuffer + offset + 2))) << (8 * 2);
-      u_battery2_voltage.base |= ((uint32_t) (*(inbuffer + offset + 3))) << (8 * 3);
-      this->battery2_voltage = u_battery2_voltage.real;
-      offset += sizeof(this->battery2_voltage);
-      union {
-        float real;
-        uint32_t base;
-      } u_battery3_voltage;
-      u_battery3_voltage.base = 0;
-      u_battery3_voltage.base |= ((uint32_t) (*(inbuffer + offset + 0))) << (8 * 0);
-      u_battery3_voltage.base |= ((uint32_t) (*(inbuffer + offset + 1))) << (8 * 1);
-      u_battery3_voltage.base |= ((uint32_t) (*(inbuffer + offset + 2))) << (8 * 2);
-      u_battery3_voltage.base |= ((uint32_t) (*(inbuffer + offset + 3))) << (8 * 3);
-      this->battery3_voltage = u_battery3_voltage.real;
-      offset += sizeof(this->battery3_voltage);
+      } u_rover_battery_voltage;
+      u_rover_battery_voltage.base = 0;
+      u_rover_battery_voltage.base |= ((uint32_t) (*(inbuffer + offset + 0))) << (8 * 0);
+      u_rover_battery_voltage.base |= ((uint32_t) (*(inbuffer + offset + 1))) << (8 * 1);
+      u_rover_battery_voltage.base |= ((uint32_t) (*(inbuffer + offset + 2))) << (8 * 2);
+      u_rover_battery_voltage.base |= ((uint32_t) (*(inbuffer + offset + 3))) << (8 * 3);
+      this->rover_battery_voltage = u_rover_battery_voltage.real;
+      offset += sizeof(this->rover_battery_voltage);
      return offset;
     }
 
     const char * getType(){ return "ric_robot/ric_status"; };
-    const char * getMD5(){ return "94718088ad2766e13cb29c6d268fcfbd"; };
+    const char * getMD5(){ return "67d6e73ce3006094432bbce95b6807f9"; };
 
   };
 

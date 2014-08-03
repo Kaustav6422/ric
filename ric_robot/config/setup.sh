7d895d528ed7d722ba7ea8bc9efe906bfe57218e
Yam Geva <yam@robotican.net>
2014-08-03 12:28:16 +0300

    fixes

--- ric_robot/config/setup.sh
+++ ric_robot/config/setup.sh
@@ -3,11 +3,16 @@ source ../../../../devel/setup.bash
 echo -e "\e[34mSetting usb rules...\e[0m"
 cp ric_usb.rules /etc/udev/rules.d
 cp 49-teensy.rules /etc/udev/rules.d/49-teensy.rules
-cp uvcvideo.conf /etc/modprobe.d
 
 echo -e "\e[34mAdding user: [$SUDO_USER] to dialout group (serial premmisions)\e[0m"
 usermod -a -G dialout $SUDO_USER
 
+echo -e "\e[32mDo you have the onboard Intel Atom CPU Dual Core 1.86GHz computer (y/n)?\e[0m"
+read
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    cp uvcvideo.conf /etc/modprobe.d
+fi
 
 echo -e "\e[34mInstalling rosserial...\e[0m"
 roscd ric_robot
@@ -20,40 +25,40 @@ echo -e "\e[32mWould you like to install usb_cam package (front webcam support) 
 read
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-    apt-get install ros-hydro-usb-cam
+    apt-get -y install ros-hydro-usb-cam
 fi
 
 echo -e "\e[32mWould you like to install openni2 packages (Asus Xtion PRO LIVE support) (y/n)?\e[0m"
 read
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-    apt-get install ros-hydro-openni2-*
+    apt-get -y install ros-hydro-openni2-*
 fi
 
 echo -e "\e[32mWould you like to install openssh packages (remote machine control support) (y/n)?\e[0m"
 read
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-    apt-get install openssh-client openssh-server
+    apt-get -y install openssh-client openssh-server
 fi
 
 echo -e "\e[32mWould you like to install hokuyo_node packages (Hokuyo laser scanner support) (y/n)?\e[0m"
 read
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-    apt-get install ros-hydro-hokuyo-node
+    apt-get -y install ros-hydro-hokuyo-node
 fi
 
 echo -e "\e[32mWould you like to install ros-hydro-dynamixel-motor packages (Komodo arm support) (y/n)?\e[0m"
 read
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-    apt-get install ros-hydro-dynamixel-motor
+    apt-get -y install ros-hydro-dynamixel-motor
 fi
 
 echo -e "\e[32mWould you like to install ros-hydro-joystick-drivers package (joystick support) (y/n)?\e[0m"
 read
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-    apt-get install ros-hydro-joystick-drivers
+    apt-get -y install ros-hydro-joystick-drivers
 fi

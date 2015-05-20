6eda57059b8cd39ff5c8c730db5a302ac2bab793
Yam Geva <yam@robotican.net>
2015-05-20 09:22:32 +0300

    fix scripts

--- ric_base_station/launch/remote_robot.sh
+++ ric_base_station/launch/remote_robot.sh
@@ -1,10 +1,21 @@
 #!/usr/bin/expect -f
 set robot_ip [lindex $argv 0]
 set pc_ip [lindex $argv 1]
-spawn ssh lizi@$robot_ip -o ServerAliveInterval=30
-expect "password:"
-send "a\r"
-expect "lizi@"
+set user [lindex $argv 2]
+set pass [lindex $argv 3]
+spawn ssh -o StrictHostKeyChecking=no $user@$robot_ip -o ServerAliveInterval=30
+
+expect {
+        "*?assword:*" {
+         send "$pass\r"
+    }
+    "No route to host" {
+        puts  "No route to host $robot_ip, check that the robot IP is correct"
+        exit
+    }
+}
+
+expect "$user@"
 send "echo '#!/bin/sh' > ~/catkin_ws/src/ric/ric_base_station/config/remote_env.sh\r"
 send "echo 'export ROS_HOSTNAME=\"$robot_ip\"' >> ~/catkin_ws/src/ric/ric_base_station/config/remote_env.sh\r"
 send "echo 'export ROS_MASTER_URI=\"http://localhost:11311\"' >> ~/catkin_ws/src/ric/ric_base_station/config/remote_env.sh\r"
@@ -14,8 +25,7 @@ send "echo 'exec \"\$@\"' >> ~/catkin_ws/src/ric/ric_base_station/config/remote_
 send "roscore\r"
 expect "started core service"
 puts "Launching remote robot..."
-system "~/catkin_ws/src/ric/ric_base_station/launch/base_station.sh $robot_ip $pc_ip"
-#interact
+system "~/catkin_ws/src/ric/ric_base_station/launch/base_station.sh $robot_ip $pc_ip $user $pass"
 while {true } {
 #	puts ".\n";
 	sleep 5

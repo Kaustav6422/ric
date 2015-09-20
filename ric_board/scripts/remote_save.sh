f9de82e170b93ddaffecf19f4c36d5860e77ae5d
tom wiss <tomwiss1231@hotmail.com>
2015-09-20 12:02:03 +0300

    update

---
+++ ric_board/scripts/remote_save.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+scp ~/catkin_ws/src/ric/ric_board/config/$1.yaml $2@$3:~/catkin_ws/src/ric/ric_board/config/
+scp ~/catkin_ws/src/ric/ric_board/DATA/$1.RIC $2@$3:~/catkin_ws/src/ric/ric_board/DATA/
+scp ~/catkin_ws/src/ric/ric_board/launch/$1.launch $2@$3:~/catkin_ws/src/ric/ric_board/launch/

e00770c862e97e2a196fd46d3042d67c905a1163
tom wiss <tomwiss1231@hotmail.com>
2015-10-16 14:26:04 +0300

    update

--- ric_robot/scripts/Arm_Move/startArm.py
+++ ric_robot/scripts/Arm_Move/startArm.py
@@ -5,7 +5,7 @@ from std_msgs.msg import Float64
 
 
 def menu():
-    return '\nPlease select a single number from the option below to move the komodo arm:\n1) Move to home position.\n2) Prepare to hit position. (not the hit itself just to prepare)\n3) Hit!.\n0) To exit this program.'
+    return '\nPlease select a single number from the option below to move the komodo arm:\n1) Move to home position.\n2) Say hi.\n3) Rotate wrist.\n4) Prepare to hit position. (not the hit itself just to prepare)\n5) Hit!.\n0) To exit this program.'
 
 
 def main():
@@ -33,6 +33,83 @@ def main():
                     komodo_arm_positions = [Float64()] * len(komodo_arm_publishers)
                     komodo_arm_positions[0].data = 0.0
                     komodo_arm_publishers[0].publish(komodo_arm_positions[0])
+                    komodo_arm_positions[1].data = 0.0
+                    komodo_arm_publishers[1].publish(komodo_arm_positions[1])
+                    komodo_arm_positions[2].data = 0.0
+                    komodo_arm_publishers[2].publish(komodo_arm_positions[2])
+                    komodo_arm_positions[3].data = 0.0
+                    komodo_arm_publishers[3].publish(komodo_arm_positions[3])
+                    komodo_arm_positions[4].data = 0.0
+                    komodo_arm_publishers[4].publish(komodo_arm_positions[4])
+                    komodo_arm_positions[5].data = 0.0
+                    komodo_arm_publishers[5].publish(komodo_arm_positions[5])
+                    komodo_arm_positions[6].data = 0.0
+                    komodo_arm_publishers[6].publish(komodo_arm_positions[6])
+                    rospy.sleep(5.0)
+
+                    komodo_arm_positions = [Float64()] * len(komodo_arm_publishers)
+                    komodo_arm_positions[0].data = 0.0
+                    komodo_arm_publishers[0].publish(komodo_arm_positions[0])
+                    komodo_arm_positions[1].data = 0.0
+                    komodo_arm_publishers[1].publish(komodo_arm_positions[1])
+                    komodo_arm_positions[2].data = 0.0
+                    komodo_arm_publishers[2].publish(komodo_arm_positions[2])
+                    komodo_arm_positions[3].data = 0.0
+                    komodo_arm_publishers[3].publish(komodo_arm_positions[3])
+                    komodo_arm_positions[4].data = 0.0
+                    komodo_arm_publishers[4].publish(komodo_arm_positions[4])
+                    komodo_arm_positions[5].data = 0.5
+                    komodo_arm_publishers[5].publish(komodo_arm_positions[5])
+                    komodo_arm_positions[6].data = 0.0
+                    komodo_arm_publishers[6].publish(komodo_arm_positions[6])
+                    rospy.sleep(3.0)
+
+                    komodo_arm_positions = [Float64()] * len(komodo_arm_publishers)
+                    komodo_arm_positions[0].data = 0.0
+                    komodo_arm_publishers[0].publish(komodo_arm_positions[0])
+                    komodo_arm_positions[1].data = 0.0
+                    komodo_arm_publishers[1].publish(komodo_arm_positions[1])
+                    komodo_arm_positions[2].data = -0.5
+                    komodo_arm_publishers[2].publish(komodo_arm_positions[2])
+                    komodo_arm_positions[3].data = 0.5
+                    komodo_arm_publishers[3].publish(komodo_arm_positions[3])
+                    komodo_arm_positions[4].data = 0.0
+                    komodo_arm_publishers[4].publish(komodo_arm_positions[4])
+                    komodo_arm_positions[5].data = -0.5
+                    komodo_arm_publishers[5].publish(komodo_arm_positions[5])
+                    komodo_arm_positions[6].data = 0.0
+                    komodo_arm_publishers[6].publish(komodo_arm_positions[6])
+                    rospy.sleep(5.0)
+
+                    komodo_arm_positions = [Float64()] * len(komodo_arm_publishers)
+                    komodo_arm_positions[0].data = 0.0
+                    komodo_arm_publishers[0].publish(komodo_arm_positions[0])
+                    komodo_arm_positions[1].data = 0.0
+                    komodo_arm_publishers[1].publish(komodo_arm_positions[1])
+                    komodo_arm_positions[2].data = 0.0
+                    komodo_arm_publishers[2].publish(komodo_arm_positions[2])
+                    komodo_arm_positions[3].data = 0.0
+                    komodo_arm_publishers[3].publish(komodo_arm_positions[3])
+                    komodo_arm_positions[4].data = 0.0
+                    komodo_arm_publishers[4].publish(komodo_arm_positions[4])
+                    komodo_arm_positions[5].data = 0.0
+                    komodo_arm_publishers[5].publish(komodo_arm_positions[5])
+                    komodo_arm_positions[6].data = 0.0
+                    komodo_arm_publishers[6].publish(komodo_arm_positions[6])
+		elif choice == 3:
+		    msg = Float64()
+		    msg.data = 1.0
+		    komodo_arm_publishers[0].publish(msg)
+		    rospy.sleep(2.0)
+		    msg = Float64()
+		    msg.data = -1.0
+		    komodo_arm_publishers[0].publish(msg)
+		    rospy.sleep(2.0)
+
+		elif choice == 4:
+		    komodo_arm_positions = [Float64()] * len(komodo_arm_publishers)
+                    komodo_arm_positions[0].data = 0.0
+                    komodo_arm_publishers[0].publish(komodo_arm_positions[0])
                     komodo_arm_positions[1].data = 0.7
                     komodo_arm_publishers[1].publish(komodo_arm_positions[1])
                     komodo_arm_positions[2].data = -0.5
@@ -46,13 +123,11 @@ def main():
                     komodo_arm_positions[6].data = -1.0
                     komodo_arm_publishers[6].publish(komodo_arm_positions[6])
 
-                    #for i in xrange(len(komodo_arm_publishers)):
-                    #    komodo_arm_publishers[i].publish(komodo_arm_positions[i])
-                    #    rospy.sleep(1.0)
-                elif choice == 3:
+                elif choice == 5:
                     msg = Float64()
                     msg.data = 1.0
                     komodo_arm_publishers[6].publish(msg)
+
                 elif choice != 0:
                     rospy.loginfo("Invalid choice, please enter your choice between 0-3.")
 

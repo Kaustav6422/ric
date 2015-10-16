77ff00d92240634990d79e9cc75e63edf87026b9
tom wiss <tomwiss1231@hotmail.com>
2015-10-16 14:37:02 +0300

    update

--- ric_robot/scripts/Arm_Move/startArm.py
+++ ric_robot/scripts/Arm_Move/startArm.py
@@ -5,7 +5,7 @@ from std_msgs.msg import Float64
 
 
 def menu():
-    return '\nPlease select a single number from the option below to move the komodo arm:\n1) Move to home position.\n2) Say hi.\n3) Rotate wrist.\n4) Prepare to hit position. (not the hit itself just to prepare)\n5) Hit!.\n0) To exit this program.'
+    return '\nPlease select a single number from the option below to move the komodo arm:\n1) Move to home position.\n2) Say hi.\n3) Rotate wrist.\n4) Prepare to hit position. (not the hit itself just to prepare)\n5) Hit!.\n6) somthing.\n0) To exit this program.'
 
 
 def main():
@@ -45,7 +45,7 @@ def main():
                     komodo_arm_publishers[5].publish(komodo_arm_positions[5])
                     komodo_arm_positions[6].data = 0.0
                     komodo_arm_publishers[6].publish(komodo_arm_positions[6])
-                    rospy.sleep(5.0)
+                    rospy.sleep(3.0)
 
                     komodo_arm_positions = [Float64()] * len(komodo_arm_publishers)
                     komodo_arm_positions[0].data = 0.0
@@ -127,9 +127,25 @@ def main():
                     msg = Float64()
                     msg.data = 1.0
                     komodo_arm_publishers[6].publish(msg)
-
+		elif choice == 6:
+    		    komodo_arm_positions = [Float64()] * len(komodo_arm_publishers)
+                    komodo_arm_positions[0].data = 0.0
+                    komodo_arm_publishers[0].publish(komodo_arm_positions[0])
+                    komodo_arm_positions[1].data = 0.0
+                    komodo_arm_publishers[1].publish(komodo_arm_positions[1])
+                    komodo_arm_positions[2].data = 0.0
+                    komodo_arm_publishers[2].publish(komodo_arm_positions[2])
+                    komodo_arm_positions[3].data = 0.0
+                    komodo_arm_publishers[3].publish(komodo_arm_positions[3])
+                    komodo_arm_positions[4].data = 0.7
+                    komodo_arm_publishers[4].publish(komodo_arm_positions[4])
+                    komodo_arm_positions[5].data = 0.7
+                    komodo_arm_publishers[5].publish(komodo_arm_positions[5])
+		    rospy.sleep(1.0)
+                    komodo_arm_positions[6].data = -1.0
+                    komodo_arm_publishers[6].publish(komodo_arm_positions[6])
                 elif choice != 0:
-                    rospy.loginfo("Invalid choice, please enter your choice between 0-3.")
+                    rospy.loginfo("Invalid choice, please enter your choice between 0-6.")
 
             except ValueError:
                 rospy.logerr('Invalid Integer, this program only except integers as choices')

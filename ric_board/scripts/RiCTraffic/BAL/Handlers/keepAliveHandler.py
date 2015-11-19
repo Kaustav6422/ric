330f33dc135ce3b76fb43d442322614364e74a73
tom wiss <tomwiss1231@hotmail.com>
2015-11-19 17:14:09 +0200

    bug fix (ppm)

--- ric_board/scripts/RiCTraffic/BAL/Handlers/keepAliveHandler.py
+++ ric_board/scripts/RiCTraffic/BAL/Handlers/keepAliveHandler.py
@@ -6,25 +6,28 @@ import subprocess
 from threading import Thread
 from BAL.Interfaces.Runnable import Runnable
 
-TIME_OUT = 3000
+TIME_OUT = 5000
 
 
 class KeepAliveHandler(Runnable):
-
     is_init = False
 
     def __init__(self, topic_name, msg_type):
-        if self.is_init: pass
-        self.is_init = True
-        self._watch_dog_time = int(round(time.time() * 1000))
-        rospy.Subscriber(topic_name, msg_type, self.callback_to_watch)
-        Thread(target=self.run, args=()).start()
+        if not KeepAliveHandler.is_init:
+            KeepAliveHandler.is_init = True
+            self._watch_dog_time = int(round(time.time() * 1000))
+            rospy.Subscriber(topic_name, msg_type, self.callback_to_watch)
+            Thread(target=self.run, args=()).start()
+        else: pass
 
     def run(self):
-        rate = rospy.Rate(20)
-        while not rospy.is_shutdown():
-            if int(round(time.time() * 1000)) - self._watch_dog_time > TIME_OUT:
+        rate = rospy.Rate(50)
+        send_err = False
+        while not rospy.is_shutdown() and not send_err:
+            if (int(round(time.time() * 1000)) - self._watch_dog_time) > TIME_OUT:
+                rospy.logerr("RiC Board is not responding")
                 subprocess.Popen(shlex.split("pkill -f ros"))
+                send_err = True
             rate.sleep()
 
     def callback_to_watch(self, msg):
